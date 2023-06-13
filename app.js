19df9557e16652f881af568918e47314f44bebee
Viktor N <viktor.nestorov@web.de>
2023-06-13 10:40:27 +0200

    Added changes to routes and views files

--- app.js
+++ app.js
@@ -27,14 +27,14 @@ app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 const indexRoutes = require("./routes/index.routes");
 app.use("/", indexRoutes);
 
-//const coursesRoutes = require("./routes/courses.routes");
-//app.use("/", coursesRoutes);
+const coursesRoutes = require("./routes/courses.routes");
+app.use("/", coursesRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-const coursesRoutes = require("./routes/courses.routes");
-app.use("/courses", coursesRoutes);
+//const coursesRoutes = require("./routes/courses.routes");
+//app.use("/courses", coursesRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -9,8 +9,101 @@ const DanceCourses = require('../models/Course.model');
 
 const isLoggedIn = require('../middleware/isLoggedIn');
 
-
-// GET
+// READ: display all courses
 router.get("/courses", (req, res, next) => {
-    res.render("create-course");
-  });
+  DanceCourses.find()
+    .then((coursesFromDB) => {
+      const data = {
+        courses: coursesFromDB,
+      };
+      res.render("dance-course/list-course", data);
+    })
+    .catch((e) => {
+      console.log("error getting list of courses from DB", e);
+      next(e);
+    });
+});
+
+// CREATE: display form
+router.get("/courses/create", isLoggedIn, (req, res, next) => {
+  res.render("dance-course/create-course");
+});
+
+// CREATE: process form
+router.post("/courses/create", isLoggedIn, (req, res, next) => {
+  const newCourse = {
+    name: req.body.name,
+    dancestyle: req.body.dancestyle,
+    teacher: req.body.teacher,
+    location: req.body.location,
+    address: req.body.address,
+    level: req.body.level,
+    price: req.body.price,
+  };
+
+  DanceCourses.create(newCourse)
+    .then((newCourse) => {
+      res.redirect("/courses");
+    })
+    .catch((e) => {
+      console.log("error creating new course", e);
+      next(e);
+    });
+});
+
+// UPDATE: display form
+router.get("/courses/:courseId/edit", isLoggedIn, async (req, res, next) => {
+  const { courseId } = req.params;
+
+  try {
+    const courseDetails = await DanceCourses.findById(courseId);
+
+    res.render("dance-course/edit-course", { course: courseDetails });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// UPDATE: process form
+router.post("/courses/:courseId/edit", isLoggedIn, (req, res, next) => {
+  const { courseId } = req.params;
+  const { name, dancestyle, teacher, location, address, level, price } = req.body;
+
+  DanceCourses.findByIdAndUpdate(courseId, { name, dancestyle, teacher, location, address, level, price }, { new: true })
+    .then((updatedCourse) => {
+      res.redirect(`/courses/${updatedCourse.id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// DELETE: delete course
+router.post("/courses/:courseId/delete", isLoggedIn, (req, res, next) => {
+  const { courseId } = req.params;
+
+  DanceCourses.findByIdAndDelete(courseId)
+    .then(() => {
+      res.redirect("/courses");
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// READ: display details of one course
+router.get("/courses/:courseId", (req, res, next) => {
+  const id = req.params.courseId;
+
+  DanceCourses.findById(id)
+    .then((courseFromDB) => {
+      res.render("dance-course/details-course", courseFromDB);
+    })
+    .catch((e) => {
+      console.log("error getting course details from DB", e);
+      next(e);
+    });
+});
+
+module.exports = router;
+
